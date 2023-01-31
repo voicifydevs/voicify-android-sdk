4aa9ea901b4ae4d0a806934d6c30636f6636b6a7
James <jmccarthy@voicify.com>
2023-01-31 16:32:47 -0500

    map configuration body props

--- src/voicify-assistant-sdk/src/main/java/com/voicify/voicify_assistant_sdk/components/HintsRecyclerViewAdapter.kt
+++ src/voicify-assistant-sdk/src/main/java/com/voicify/voicify_assistant_sdk/components/HintsRecyclerViewAdapter.kt
@@ -29,17 +29,17 @@ internal class HintsRecyclerViewAdapter(private var hintsList: List<String>, pri
         val hintsContainer = holder.hintsContainerLayout
         val hintTextView = holder.hintsTextView
         val hintsContainerLayoutStyle = GradientDrawable()
-        hintsContainerLayoutStyle.cornerRadius = bodyProps?.hintsBorderRadius ?: 34f
-        hintsContainerLayoutStyle.setStroke(bodyProps?.hintsBorderWidth ?: 4, Color.parseColor(bodyProps?.hintsBorderColor ?: "#CBCCD2"))
-        hintsContainerLayoutStyle.setColor(Color.parseColor(bodyProps?.hintsBackgroundColor ?: "#ffffff"))
+        hintsContainerLayoutStyle.cornerRadius = bodyProps?.hintsBorderRadius ?: configurationBodyProps?.hintsBorderRadius ?: 34f
+        hintsContainerLayoutStyle.setStroke(bodyProps?.hintsBorderWidth ?: configurationBodyProps?.hintsBorderWidth ?: 4, Color.parseColor(bodyProps?.hintsBorderColor ?: configurationBodyProps?.hintsBorderColor ?: "#CBCCD2"))
+        hintsContainerLayoutStyle.setColor(Color.parseColor(bodyProps?.hintsBackgroundColor ?: configurationBodyProps?.hintsBackgroundColor ?: "#ffffff"))
         hintsContainer.background = hintsContainerLayoutStyle
         val layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT)
         layoutParams.setMargins(16, 0 , 0, 0)
         hintsContainer.layoutParams = layoutParams
-        hintsContainer.setPadding(bodyProps?.hintsPaddingLeft ?: 40,bodyProps?.hintsPaddingTop ?: 20,bodyProps?.hintsPaddingRight ?: 40,bodyProps?.hintsPaddingBottom ?: 20,)
-        hintTextView.textSize = bodyProps?.hintsFontSize ?: 14f
-        hintTextView.typeface = Typeface.create(bodyProps?.hintsFontFamily ?: "sans-serif", Typeface.NORMAL)
-        hintTextView.setTextColor(Color.parseColor(bodyProps?.hintsTextColor ?: "#CBCCD2"))
+        hintsContainer.setPadding(bodyProps?.hintsPaddingLeft ?: configurationBodyProps?.hintsPaddingLeft ?: 40,bodyProps?.hintsPaddingTop ?: configurationBodyProps?.hintsPaddingTop ?: 20,bodyProps?.hintsPaddingRight ?: configurationBodyProps?.hintsPaddingRight ?: 40,bodyProps?.hintsPaddingBottom ?: configurationBodyProps?.hintsPaddingBottom ?: 20,)
+        hintTextView.textSize = bodyProps?.hintsFontSize ?: configurationBodyProps?.hintsFontSize ?: 14f
+        hintTextView.typeface = Typeface.create(bodyProps?.hintsFontFamily ?: configurationBodyProps?.hintsFontFamily ?: "sans-serif", Typeface.NORMAL)
+        hintTextView.setTextColor(Color.parseColor(bodyProps?.hintsTextColor ?: configurationBodyProps?.hintsTextColor ?: "#CBCCD2"))
         hintTextView.text = hint
         hintsContainer.setOnClickListener{
             onHintClicked(hint)
